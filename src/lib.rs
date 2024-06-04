91b9a219d5d648b77c05f77a7ea60433e86efc18
Charles Schaefer <charlesschaefer@gmail.com>
2024-06-04 10:12:37 -0300

    Changing the library to return JSON objects instead of strings

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use reqwest::{Client, Error};
 use std::env;
+use serde_json;
 
 pub struct ValorantAPI {
     api_key: String,
@@ -16,7 +17,7 @@ impl ValorantAPI {
         }
     }
 
-    pub async fn request(&self, method: String) -> Result<String, Error> {
+    pub async fn request(&self, method: String) -> Result<serde_json::Value, Error> {
         let mut url = self.base_url.clone();
         url.push_str(method.as_str());
         //url.push_str("?api_key=");
@@ -34,24 +35,27 @@ impl ValorantAPI {
             .text()
             .await?;
     
-        Ok(res)
+        let json: serde_json::Value = serde_json::from_str(res.as_str())
+            .expect("Couldn't parse json");
+    
+        Ok(json)
     }
 
-    pub async fn status(&self) -> String {
+    pub async fn status(&self) -> serde_json::Value {
         let method = "val/status/v1/platform-data".to_string();
         let response = self.request(method).await;
 
         response.unwrap()
     }
 
-    pub async fn content(&self) -> String {
+    pub async fn content(&self) -> serde_json::Value {
         let method = "val/content/v1/contents?locale=pt-BR".to_string();
         let response = self.request(method).await;
 
         response.unwrap()
     }
 
-    pub async fn ranked_by_act(&self, act_id: String) -> String {
+    pub async fn ranked_by_act(&self, act_id: String) -> serde_json::Value {
         let mut method = "val/ranked/v1/leaderboards/by-act/".to_string();
         method.push_str(&act_id.as_str());
 
@@ -60,7 +64,7 @@ impl ValorantAPI {
         response.unwrap()
     }
 
-    pub async fn get_match(&self, match_id: String) -> String {
+    pub async fn get_match(&self, match_id: String) -> serde_json::Value {
         let mut method = "val/match/v1/matches/".to_string();
         method.push_str(&match_id.as_str());
 
@@ -69,7 +73,7 @@ impl ValorantAPI {
         response.unwrap()
     }
 
-    pub async fn matches_from_user(&self, user_id: String) -> String {
+    pub async fn matches_from_user(&self, user_id: String) -> serde_json::Value {
         let mut method = "val/match/v1/matchlists/by-puuid/".to_string();
         method.push_str(&user_id.as_str());
 
@@ -78,7 +82,7 @@ impl ValorantAPI {
         response.unwrap()
     }
 
-    pub async fn matches_by_queue(&self, queue_id: String) -> String {
+    pub async fn matches_by_queue(&self, queue_id: String) -> serde_json::Value {
         let mut method = "val/match/v1/recent-matches/by-queue/".to_string();
         method.push_str(&queue_id.as_str());
 
